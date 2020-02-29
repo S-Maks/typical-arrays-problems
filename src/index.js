d67f57c4c69844c733decd64dfa116ab6aa2318f
Максим Титок <cool.dan47@mail.ru>
2020-02-29 22:03:06 +0300

    feat: add finding function max, min, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined) return 0;
+  if(array.length == 0) return 0;  
+
+ 
+  var minElement = array[0];
+  for(let i = 0; i<array.length; i++){
+    if(minElement>array[i]){
+      minElement= array[i];
+    }
+  }
+  return minElement;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array===undefined)return 0;
+  if(array.length ==0) return 0;
+
+  var maxElement = array[0];
+  for(let i = 0; i<array.length; i++){
+    if(maxElement<array[i]){
+      maxElement= array[i];
+    }
+  }
+  return maxElement;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array===undefined)return 0;
+  if(array.length ==0) return 0;
+  var sum = 0;
+  for(let i = 0; i<array.length; i++){
+    sum+=array[i];
+  }
+  return sum/array.length;
 }
